ed80b070421ef215c2466e4f7a08ff602adc0760
brett <brett@madebygrizzly.com>
2016-04-09 14:23:43 -0700

    adjusts title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html class="no-js" lang="">
     <head>
-        <title>Weather Vue</title>
+        <title>Weather Tab</title>
         <?php include('functions.php'); ?>
         <?php include_once('partials/head.php'); ?>
     </head>
@@ -56,6 +56,17 @@
 
         <?php include_once('partials/footer.php') ?>
         <?php include_once('partials/foot.php') ?>
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-30440788-2', 'auto');
+          ga('send', 'pageview');
+
+        </script>        
 
     </body>
 </html>
